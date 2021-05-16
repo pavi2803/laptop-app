33b71779ab2248855f0ac4b9a882b93d38906272
Pavithra S <pavi2468kuk@gmail.com>
2021-05-16 16:12:42 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -85,4 +85,7 @@ def fetch_details_cpu(a,b,c,d):
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)     
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+        }); 
+    app.run()
